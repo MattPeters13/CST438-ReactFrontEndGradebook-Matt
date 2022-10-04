a3caf33b4128f0e989be99fb4efa9f182099f41c
MattPeters13 <mapeters@csumb.edu>
2022-10-04 11:38:50 -0700

    implement feature add assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import AddAssignment from './components/AddAssignment';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Gradebook from './components/Gradebook';
 import Assignment from './components/Assignment';
 import {BrowserRouter, Switch,  Route} from 'react-router-dom';
+import React from 'react';
 
 function App() {
+
+  const [assignment, setAssignment] = React.useState([]);
+
+  const addAssignment = (assignment) => {
+    setAssignment([assignment, ...assignment]);
+  }
+
+
   return (
     <div className="App">
       <AppBar position="static" color="default">
@@ -21,7 +31,8 @@ function App() {
         <Route exact path='/' component={Assignment} />
         <Route path='/gradebook' component={Gradebook} />
        </Switch>
-      </BrowserRouter>
+      </BrowserRouter>       
+      <AddAssignment addAssignment={addAssignment} />
     </div>
   );
 }
